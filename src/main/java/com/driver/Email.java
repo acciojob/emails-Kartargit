21bc65792ac0f6916ecd4d84aa73dda1a127fb13
Kartargit <skartar61962@gmail.com>
2023-07-22 23:09:21 +0530

    eGmail

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -4,10 +4,19 @@ public class Email {
 
     private String emailId;
     private String password;
-
+    public Email(){
+    }
     public Email(String emailId){
         this.emailId = emailId;
         this.password = "Accio@123";
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getEmailId() {
@@ -25,5 +34,46 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        String currPassword = getPassword();
+        if(currPassword.equals(oldPassword)){
+            int n = newPassword.length();
+            if(n<8){
+                System.out.println("Entered Password length is less than 8");
+                return;
+            } else if (n>=8) {
+                if(isValid(newPassword)){
+                    setPassword(newPassword);
+                    System.out.println("Password has been changed successfully");
+                    return;
+                }
+
+                System.out.println("Entered Password is inValid");
+                return;
+
+            }
+        }
+            System.out.println("Entered Password does not match with current password");
+            return;
+
+    }
+    public static boolean isValid(String password){
+        boolean upCase = false, lowCase = false, digit = false, specChar = false;
+        for(int i=0;i<password.length();i++){
+            int asc = (int)password.charAt(i);
+            if(!specChar){
+                if((asc<=47&&asc>=33)||(asc<=64&&asc>=58)||(asc<=96&&asc>=91)||(asc<=126&&asc>=123))specChar = true;
+            }
+            if(!digit){
+                if(asc>=48&&asc<=57)digit = true;
+            }
+            if(!upCase){
+                if(asc>=65&&asc<=90)upCase = true;
+            }
+            if(!lowCase){
+                if(asc>=97&&asc<=122)lowCase = true;
+            }
+            if(specChar&&digit&&lowCase&&upCase)return true;
+        }
+        return false;
     }
 }
--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -5,18 +5,35 @@ import org.apache.commons.lang3.tuple.Pair;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class Workspace extends Gmail{
 
     private ArrayList<Meeting> calendar; // Stores all the meetings
 
+    public void setCalendar(ArrayList<Meeting> calendar) {
+        this.calendar = calendar;
+    }
+
+    public ArrayList<Meeting> getCalendar() {
+        return calendar;
+    }
+
     public Workspace(String emailId) {
         // The inboxCapacity is equal to the maximum value an integer can store.
-
+        super(emailId,Integer.MAX_VALUE);
     }
 
     public void addMeeting(Meeting meeting){
         //add the meeting to calendar
+//        calendar = new ArrayList<>();
+        if(calendar==null){
+            calendar = new ArrayList<>();
+            calendar.add(meeting);
+        }
+        else{
+            calendar.add(meeting);
+        }
 
     }
 
@@ -26,5 +43,16 @@ public class Workspace extends Gmail{
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
 
+        int maxMeet = 0;
+
+        LocalTime last = calendar.get(0).getEndTime();
+        for(Meeting m: calendar){
+//            LocalTime start = m.getStartTime();
+            if(last.compareTo(m.getStartTime())<0){
+                maxMeet++;
+                last = m.getStartTime();
+            }
+        }
+        return maxMeet;
     }
 }
